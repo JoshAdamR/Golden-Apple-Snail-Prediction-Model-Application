9b3867d61e2c356bf9b79fbf6f227033e260ae44
Joshua Adampin Rugag <94948671+JoshAdamR@users.noreply.github.com>
2024-11-11 23:44:48 +0800

    Update Model_App.py

--- Model_App.py
+++ Model_App.py
@@ -49,7 +49,7 @@ if option == "Single Prediction":
             juvenile = 0
         if adult < 0:
             adult = 0
-        st.success(f"Predicted Values:\n - Baby Snails: {np.round(baby)}\n - Juvenile Snails: {np.round(juvenile)}\n - Adult Snails: {np.round(adult)}\n - TOTAL: {np.round(prediction.sum())}")
+        st.success(f"Predicted Values:\n - Baby Snails: {np.round(baby)}\n - Juvenile Snails: {np.round(juvenile)}\n - Adult Snails: {np.round(adult)}\n - TOTAL: {np.round(np.round(baby)+np.round(juvenile)+np.round(adult))}")
 
 # Batch Prediction via File Upload
 elif option == "Batch Prediction (Upload File)":
